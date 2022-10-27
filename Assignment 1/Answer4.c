3dd65918219bcde57c74da60b1e262ff9f7f0586
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-10-27 19:45:11 +0530

    Create Answer4.c

---
+++ Assignment 1/Answer4.c
@@ -0,0 +1,7 @@
+/*5. WAP to calculate the length of String using printf function.*/
+#include<stdio.h>
+int main()
+{
+    int x =printf("Mohd Junaid");
+    printf("\nLength of your string :%d",x);
+}
